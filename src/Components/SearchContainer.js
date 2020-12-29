052f8a6e901e40c9d02e8b2ed27f8722094777d0
Khusharth <khusharth19@gmail.com>
2020-12-30 05:06:08 +0530

    feat: Adding Search Container Component

---
+++ src/Components/SearchContainer.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import {
+    attributeOptions,
+    cityOptions,
+    venueOptions,
+    winnerOptions,
+    seasonOptions,
+} from "../Utils/filterOptions";
+
+// Styles for Select
+const customStyles = {
+    control: (provided) => ({
+        ...provided,
+        boxShadow: "0 0 2px #047857",
+    }),
+    option: (provided, { isSelected, isDisabled, isFocused }) => ({
+        ...provided,
+        backgroundColor: isSelected ? " #059669" : isFocused ? "#a7f3d0" : null,
+        ":active": {
+            ...provided[":active"],
+            backgroundColor:
+                !isDisabled && (isSelected ? "#059669" : "#a7f3d0"),
+        },
+    }),
+};
+
+const setOptions = (attribute, setAttrValueOptions, setAttributeValue) => {
+    switch (attribute) {
+        case "all":
+            setAttrValueOptions([]);
+            setAttributeValue("");
+            break;
+
+        case "Season":
+            setAttrValueOptions(seasonOptions);
+            setAttributeValue(seasonOptions[0]);
+            break;
+
+        case "city":
+            setAttrValueOptions(cityOptions);
+            setAttributeValue(cityOptions[0]);
+            break;
+
+        case "venue":
+            setAttrValueOptions(venueOptions);
+            setAttributeValue(venueOptions[0]);
+            break;
+
+        case "winner":
+            setAttrValueOptions(winnerOptions);
+            setAttributeValue(winnerOptions[0]);
+            break;
+
+        default:
+            setAttrValueOptions([]);
+            break;
+    }
+};
+
+const SearchContainer = ({
+    searchText,
+    setSearchText,
+    attribute,
+    setAttribute,
+    attributeValue,
+    setAttributeValue,
+}) => {
+    const [attributeValueOptions, setAttrValueOptions] = useState([]);
+
+    useEffect(() => {
+        console.log("called");
+        setOptions(attribute.value, setAttrValueOptions, setAttributeValue);
+    }, [attribute]);
+
+    return (
+        <div className='search'>
+            <input
+                type='text'
+                name='form'
+                className='search__input'
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                placeholder='Search by team name or city'
+            />
+            <Select
+                className='filter__attribute'
+                value={attribute}
+                onChange={setAttribute}
+                options={attributeOptions}
+                styles={customStyles}
+                placeholder='Filter By Attribute'
+            />
+
+            <span className='filter__equal-sign'>=</span>
+
+            <Select
+                className='filter__attribute'
+                value={attributeValue}
+                onChange={setAttributeValue}
+                options={attributeValueOptions}
+                styles={customStyles}
+                placeholder='All'
+            />
+        </div>
+    );
+};
+
+export default SearchContainer;
---
+++ src/Utils/filterOptions.js
@@ -0,0 +1,419 @@
+export const attributeOptions = [
+    { value: "all", label: "All" },
+    { value: "Season", label: "Season" },
+    { value: "city", label: "City" },
+    { value: "winner", label: "Winner" },
+    { value: "venue", label: "Venue" },
+];
+
+export const cityOptions = [
+    {
+        value: "Hyderabad",
+        label: "Hyderabad",
+    },
+    {
+        value: "Pune",
+        label: "Pune",
+    },
+    {
+        value: "Rajkot",
+        label: "Rajkot",
+    },
+    {
+        value: "Indore",
+        label: "Indore",
+    },
+    {
+        value: "Bangalore",
+        label: "Bangalore",
+    },
+    {
+        value: "Mumbai",
+        label: "Mumbai",
+    },
+    {
+        value: "Kolkata",
+        label: "Kolkata",
+    },
+    {
+        value: "Delhi",
+        label: "Delhi",
+    },
+    {
+        value: "Chandigarh",
+        label: "Chandigarh",
+    },
+    {
+        value: "Kanpur",
+        label: "Kanpur",
+    },
+    {
+        value: "Jaipur",
+        label: "Jaipur",
+    },
+    {
+        value: "Chennai",
+        label: "Chennai",
+    },
+    {
+        value: "Cape Town",
+        label: "Cape Town",
+    },
+    {
+        value: "Port Elizabeth",
+        label: "Port Elizabeth",
+    },
+    {
+        value: "Durban",
+        label: "Durban",
+    },
+    {
+        value: "Centurion",
+        label: "Centurion",
+    },
+    {
+        value: "East London",
+        label: "East London",
+    },
+    {
+        value: "Johannesburg",
+        label: "Johannesburg",
+    },
+    {
+        value: "Kimberley",
+        label: "Kimberley",
+    },
+    {
+        value: "Bloemfontein",
+        label: "Bloemfontein",
+    },
+    {
+        value: "Ahmedabad",
+        label: "Ahmedabad",
+    },
+    {
+        value: "Cuttack",
+        label: "Cuttack",
+    },
+    {
+        value: "Nagpur",
+        label: "Nagpur",
+    },
+    {
+        value: "Dharamsala",
+        label: "Dharamsala",
+    },
+    {
+        value: "Kochi",
+        label: "Kochi",
+    },
+    {
+        value: "Visakhapatnam",
+        label: "Visakhapatnam",
+    },
+    {
+        value: "Raipur",
+        label: "Raipur",
+    },
+    {
+        value: "Ranchi",
+        label: "Ranchi",
+    },
+    {
+        value: "Abu Dhabi",
+        label: "Abu Dhabi",
+    },
+    {
+        value: "Sharjah",
+        label: "Sharjah",
+    },
+    {
+        value: "Mohali",
+        label: "Mohali",
+    },
+    {
+        value: "Bengaluru",
+        label: "Bengaluru",
+    },
+];
+
+export const venueOptions = [
+    {
+        value: "Rajiv Gandhi International Stadium, Uppal",
+        label: "Rajiv Gandhi International Stadium, Uppal",
+    },
+    {
+        value: "Maharashtra Cricket Association Stadium",
+        label: "Maharashtra Cricket Association Stadium",
+    },
+    {
+        value: "Saurashtra Cricket Association Stadium",
+        label: "Saurashtra Cricket Association Stadium",
+    },
+    {
+        value: "Holkar Cricket Stadium",
+        label: "Holkar Cricket Stadium",
+    },
+    {
+        value: "M Chinnaswamy Stadium",
+        label: "M Chinnaswamy Stadium",
+    },
+    {
+        value: "Wankhede Stadium",
+        label: "Wankhede Stadium",
+    },
+    {
+        value: "Eden Gardens",
+        label: "Eden Gardens",
+    },
+    {
+        value: "Feroz Shah Kotla",
+        label: "Feroz Shah Kotla",
+    },
+    {
+        value: "Punjab Cricket Association IS Bindra Stadium, Mohali",
+        label: "Punjab Cricket Association IS Bindra Stadium, Mohali",
+    },
+    {
+        value: "Green Park",
+        label: "Green Park",
+    },
+    {
+        value: "Punjab Cricket Association Stadium, Mohali",
+        label: "Punjab Cricket Association Stadium, Mohali",
+    },
+    {
+        value: "Sawai Mansingh Stadium",
+        label: "Sawai Mansingh Stadium",
+    },
+    {
+        value: "MA Chidambaram Stadium, Chepauk",
+        label: "MA Chidambaram Stadium, Chepauk",
+    },
+    {
+        value: "Dr DY Patil Sports Academy",
+        label: "Dr DY Patil Sports Academy",
+    },
+    {
+        value: "Newlands",
+        label: "Newlands",
+    },
+    {
+        value: "St George's Park",
+        label: "St George's Park",
+    },
+    {
+        value: "Kingsmead",
+        label: "Kingsmead",
+    },
+    {
+        value: "SuperSport Park",
+        label: "SuperSport Park",
+    },
+    {
+        value: "Buffalo Park",
+        label: "Buffalo Park",
+    },
+    {
+        value: "New Wanderers Stadium",
+        label: "New Wanderers Stadium",
+    },
+    {
+        value: "De Beers Diamond Oval",
+        label: "De Beers Diamond Oval",
+    },
+    {
+        value: "OUTsurance Oval",
+        label: "OUTsurance Oval",
+    },
+    {
+        value: "Brabourne Stadium",
+        label: "Brabourne Stadium",
+    },
+    {
+        value: "Sardar Patel Stadium, Motera",
+        label: "Sardar Patel Stadium, Motera",
+    },
+    {
+        value: "Barabati Stadium",
+        label: "Barabati Stadium",
+    },
+    {
+        value: "Vidarbha Cricket Association Stadium, Jamtha",
+        label: "Vidarbha Cricket Association Stadium, Jamtha",
+    },
+    {
+        value: "Himachal Pradesh Cricket Association Stadium",
+        label: "Himachal Pradesh Cricket Association Stadium",
+    },
+    {
+        value: "Nehru Stadium",
+        label: "Nehru Stadium",
+    },
+    {
+        value: "Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium",
+        label: "Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium",
+    },
+    {
+        value: "Subrata Roy Sahara Stadium",
+        label: "Subrata Roy Sahara Stadium",
+    },
+    {
+        value: "Shaheed Veer Narayan Singh International Stadium",
+        label: "Shaheed Veer Narayan Singh International Stadium",
+    },
+    {
+        value: "JSCA International Stadium Complex",
+        label: "JSCA International Stadium Complex",
+    },
+    {
+        value: "Sheikh Zayed Stadium",
+        label: "Sheikh Zayed Stadium",
+    },
+    {
+        value: "Sharjah Cricket Stadium",
+        label: "Sharjah Cricket Stadium",
+    },
+    {
+        value: "Dubai International Cricket Stadium",
+        label: "Dubai International Cricket Stadium",
+    },
+    {
+        value: "M. A. Chidambaram Stadium",
+        label: "M. A. Chidambaram Stadium",
+    },
+    {
+        value: "Feroz Shah Kotla Ground",
+        label: "Feroz Shah Kotla Ground",
+    },
+    {
+        value: "M. Chinnaswamy Stadium",
+        label: "M. Chinnaswamy Stadium",
+    },
+    {
+        value: "Rajiv Gandhi Intl. Cricket Stadium",
+        label: "Rajiv Gandhi Intl. Cricket Stadium",
+    },
+    {
+        value: "IS Bindra Stadium",
+        label: "IS Bindra Stadium",
+    },
+    {
+        value: "ACA-VDCA Stadium",
+        label: "ACA-VDCA Stadium",
+    },
+];
+
+export const winnerOptions = [
+    {
+        value: "Sunrisers Hyderabad",
+        label: "Sunrisers Hyderabad",
+    },
+    {
+        value: "Rising Pune Supergiant",
+        label: "Rising Pune Supergiant",
+    },
+    {
+        value: "Kolkata Knight Riders",
+        label: "Kolkata Knight Riders",
+    },
+    {
+        value: "Kings XI Punjab",
+        label: "Kings XI Punjab",
+    },
+    {
+        value: "Royal Challengers Bangalore",
+        label: "Royal Challengers Bangalore",
+    },
+    {
+        value: "Mumbai Indians",
+        label: "Mumbai Indians",
+    },
+    {
+        value: "Delhi Daredevils",
+        label: "Delhi Daredevils",
+    },
+    {
+        value: "Gujarat Lions",
+        label: "Gujarat Lions",
+    },
+    {
+        value: "Chennai Super Kings",
+        label: "Chennai Super Kings",
+    },
+    {
+        value: "Rajasthan Royals",
+        label: "Rajasthan Royals",
+    },
+    {
+        value: "Deccan Chargers",
+        label: "Deccan Chargers",
+    },
+    {
+        value: "Pune Warriors",
+        label: "Pune Warriors",
+    },
+    {
+        value: "Kochi Tuskers Kerala",
+        label: "Kochi Tuskers Kerala",
+    },
+    {
+        value: "Rising Pune Supergiants",
+        label: "Rising Pune Supergiants",
+    },
+    {
+        value: "Delhi Capitals",
+        label: "Delhi Capitals",
+    },
+];
+
+export const seasonOptions = [
+    {
+        value: "IPL-2008",
+        label: "IPL-2008",
+    },
+    {
+        value: "IPL-2009",
+        label: "IPL-2009",
+    },
+    {
+        value: "IPL-2010",
+        label: "IPL-2010",
+    },
+    {
+        value: "IPL-2011",
+        label: "IPL-2011",
+    },
+    {
+        value: "IPL-2012",
+        label: "IPL-2012",
+    },
+    {
+        value: "IPL-2013",
+        label: "IPL-2013",
+    },
+    {
+        value: "IPL-2014",
+        label: "IPL-2014",
+    },
+    {
+        value: "IPL-2015",
+        label: "IPL-2015",
+    },
+    {
+        value: "IPL-2016",
+        label: "IPL-2016",
+    },
+    {
+        value: "IPL-2017",
+        label: "IPL-2017",
+    },
+    {
+        value: "IPL-2018",
+        label: "IPL-2018",
+    },
+    {
+        value: "IPL-2019",
+        label: "IPL-2019",
+    },
+];
